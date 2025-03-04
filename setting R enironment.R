8f824e4e5fe70d957bcf7764de537bae5280623a
a1n2g3e4l5a6 <angelaamassay@gmail.com>
2025-03-04 11:30:48 +0300

    Add files via upload

---
+++ setting R enironment.R
@@ -0,0 +1,13 @@
+###setting R-Environment
+
+install.packages("renv")
+renv::init()
+install.packages("tidyverse")
+install.packages("psych")
+install.packages("vegan")
+install.packages("terra")
+install.packages("raster")
+
+renv::snapshot()
+renv::restore()
+summary(tidyverse)
